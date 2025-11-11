3597faa159dfba307569bc839e8e03f57e1d221d
Vitalij Berdinskih <vitalij_r2@outlook.com>
2025-11-11 21:57:35 +0200

    Save state

--- src/main/java/io/gitlab/vitalijr2/ridecost/cli/RideCost.java
+++ src/main/java/io/gitlab/vitalijr2/ridecost/cli/RideCost.java
@@ -20,15 +20,22 @@
 package io.gitlab.vitalijr2.ridecost.cli;
 
 import static java.util.Objects.isNull;
+import static java.util.Objects.nonNull;
 
 import io.gitlab.vitalijr2.ridecost.estimator.RideCostEstimator;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.lang.System.Logger;
 import java.lang.System.Logger.Level;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 import java.util.Objects;
+import java.util.Properties;
 import java.util.ResourceBundle;
 import java.util.stream.Stream;
+import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.VisibleForTesting;
 import picocli.CommandLine;
 import picocli.CommandLine.ArgGroup;
@@ -68,6 +75,19 @@ public class RideCost implements Runnable {
   @Option(names = "-4", descriptionKey = "round.four")
   boolean fourDigits;
 
+  @Option(names = {"--save", "-s"}, descriptionKey = "state.save")
+  boolean saveState;
+
+  public RideCost() {
+    mileage = new Mileage();
+
+    try {
+      loadState();
+    } catch (IOException exception) {
+      LOGGER.log(Level.WARNING, exception.getMessage());
+    }
+  }
+
   public static void main(String[] args) {
     var commandLine = new CommandLine(new RideCost());
 
@@ -75,13 +95,34 @@ public class RideCost implements Runnable {
     System.exit(commandLine.execute(args));
   }
 
+  @VisibleForTesting
+  @NotNull
+  static File getStateFile() {
+    return getStateFile("XDG_STATE_HOME");
+  }
+
+  @VisibleForTesting
+  @NotNull
+  static File getStateFile(String stateFolderName) {
+    var stateFolder = System.getenv().getOrDefault(stateFolderName, System.getProperty("user.home") + "/.local/state");
+
+    return new File(stateFolder, "ridecost.properties");
+  }
+
   @Override
   public void run() {
     validatePositiveDecimals();
     System.out.println(estimateRideCost());
+
+    try {
+      saveState();
+    } catch (IOException exception) {
+      LOGGER.log(Level.WARNING, exception.getMessage());
+    }
   }
 
   @VisibleForTesting
+  @NotNull
   BigDecimal estimateRideCost() {
     BigDecimal cost;
 
@@ -109,6 +150,111 @@ public class RideCost implements Runnable {
     }
 
     return cost;
+  }
+
+  private void loadState() throws IOException {
+    var stateFile = getStateFile();
+
+    if (!stateFile.exists()) {
+      return;
+    }
+
+    var stateProperties = new Properties();
+
+    stateProperties.load(new FileReader(stateFile));
+    restoreMileage(stateProperties);
+    restorePrice(stateProperties);
+    restoreRounding(stateProperties);
+  }
+
+  private void restoreMileage(Properties stateProperties) {
+    if (stateProperties.containsKey("distancePerVolume")) {
+      mileage.distancePerVolume = new BigDecimal(stateProperties.getProperty("distancePerVolume"));
+      LOGGER.log(Level.DEBUG, "Saved distance per volume: {0}", mileage.distancePerVolume);
+    } else if (stateProperties.containsKey("volumePerDistance")) {
+      mileage.volumePerDistance = new BigDecimal(stateProperties.getProperty("volumePerDistance"));
+      LOGGER.log(Level.DEBUG, "Saved volume per distance: {0}", mileage.volumePerDistance);
+    }
+  }
+
+  private void restorePrice(Properties stateProperties) {
+    if (stateProperties.containsKey("price")) {
+      price = new BigDecimal(stateProperties.getProperty("price"));
+      LOGGER.log(Level.DEBUG, "Saved price: {0}", price);
+    }
+  }
+
+  private void restoreRounding(Properties stateProperties) {
+    switch (stateProperties.getProperty("roundTo", "*")) {
+      case "0" -> {
+        zeroDigits = true;
+        LOGGER.log(Level.DEBUG, "Restore rounding to zero digits");
+      }
+      case "2" -> {
+        twoDigits = true;
+        LOGGER.log(Level.DEBUG, "Restore rounding to two digits");
+      }
+      case "3" -> {
+        threeDigits = true;
+        LOGGER.log(Level.DEBUG, "Restore rounding to three digits");
+      }
+      case "4" -> {
+        fourDigits = true;
+        LOGGER.log(Level.DEBUG, "Restore rounding to four digits");
+      }
+      default -> LOGGER.log(Level.DEBUG, "Exact value is used");
+    }
+  }
+
+  private void saveMileage(Properties stateProperties) {
+    if (nonNull(mileage.distancePerVolume)) {
+      stateProperties.setProperty("distancePerVolume", mileage.distancePerVolume.toString());
+      LOGGER.log(Level.DEBUG, "Save distance per volume: {0}", mileage.distancePerVolume);
+    } else if (nonNull(mileage.volumePerDistance)) {
+      stateProperties.setProperty("volumePerDistance", mileage.volumePerDistance.toString());
+      LOGGER.log(Level.DEBUG, "Save volume per distance: {0}", mileage.volumePerDistance);
+    }
+  }
+
+  private void savePrice(Properties stateProperties) {
+    stateProperties.setProperty("price", price.toString());
+    LOGGER.log(Level.DEBUG, "Save price: {0}", price);
+  }
+
+  private void saveRounding(Properties stateProperties) {
+    if (zeroDigits) {
+      stateProperties.put("roundTo", "0");
+      LOGGER.log(Level.DEBUG, "Save rounding to zero digits");
+    } else if (twoDigits) {
+      stateProperties.put("roundTo", "2");
+      LOGGER.log(Level.DEBUG, "Save rounding to two digits");
+    } else if (threeDigits) {
+      stateProperties.put("roundTo", "3");
+      LOGGER.log(Level.DEBUG, "Save rounding to three digits");
+    } else if (fourDigits) {
+      stateProperties.put("roundTo", "4");
+      LOGGER.log(Level.DEBUG, "Save rounding to four digits");
+    }
+  }
+
+  private void saveState() throws IOException {
+    var stateFile = getStateFile();
+
+    if (stateFile.createNewFile()) {
+      LOGGER.log(Level.DEBUG, "New state file created: {0}", stateFile.getCanonicalFile());
+    } else if (stateFile.canWrite()) {
+      LOGGER.log(Level.DEBUG, "Re-use state file: {0}", stateFile.getCanonicalFile());
+    } else {
+      LOGGER.log(Level.WARNING, "Cannot write to the state file: {0}", stateFile.getCanonicalFile());
+      return;
+    }
+
+    var stateProperties = new Properties();
+
+    saveMileage(stateProperties);
+    savePrice(stateProperties);
+    saveRounding(stateProperties);
+    stateProperties.store(new FileWriter(stateFile), "ridecost");
   }
 
   private void validatePositiveDecimals() {
